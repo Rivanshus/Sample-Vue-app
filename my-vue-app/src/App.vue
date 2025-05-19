50aa531fd2791e9aced7a0423171b8308dc703c5
Rivanshu Singla (MAQ LLC) <v-rivasingla@microsoft.com>
2025-05-19 11:58:36 +0530

    Added spacing to each of the components

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-m-4">
+  <div class="p-m-4 componentsContainer">
       <UserTable />
       <br>
       <hr/>
@@ -16,4 +16,20 @@ import UserTable from './components/UserTable.vue'
 import RoleForm from './components/RoleForm.vue';
 import TaskListTemp from './components/TaskListTemp.vue';
 </script>
+
+<style scoped>
+.componentsContainer{
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+}
+
+hr {
+  border: none;
+  height: 2px;
+  background-color: #ccc; /* or any color you prefer */
+  width: 100%;
+  margin: 20px 0;
+}
+</style>
 
